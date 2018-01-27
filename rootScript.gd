8cb0a8ae834789d4dd9b94103a618bddc1e13732
Nathaniel Tan <ntan@olin.edu>
2018-01-26 23:08:51 -0500

    Initing project

---
+++ rootScript.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var dir = Vector2(0.0, 0.0)
+func _process(delta):
+	var block_pos = get_node("testBlock").get_pos()
+	dir = Vector2(0.0, 0.0)
+	if (Input.is_action_pressed("move_down")):
+		dir.y = 1;
+	elif (Input.is_action_pressed("move_up")):
+		dir.y = -1;
+	elif (Input.is_action_pressed("move_right")):
+		dir.x = 1;
+	elif (Input.is_action_pressed("move_left")):
+		dir.x = -1;
+		
+	block_pos += dir*80*delta
+	get_node("testBlock").set_pos(block_pos)
+	
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process(true)
