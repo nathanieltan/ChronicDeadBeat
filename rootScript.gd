1973b975a8cbd23edbc711c5a2fb5aa3978c1687
Jeremy Ryan <jryan@olin.edu>
2018-01-27 10:00:44 -0500

    basic movement

--- rootScript.gd
+++ rootScript.gd
@@ -4,21 +4,34 @@ extends Node2D
 # var a = 2
 # var b = "textvar"
 var dir = Vector2(0.0, 0.0)
+var actionTaken = false;
+var size;
 func _process(delta):
-	var block_pos = get_node("Player").get_pos()
+	var kinebody = get_node("Player/KinematicBody2D")
 	dir = Vector2(0.0, 0.0)
-	if (Input.is_action_pressed("move_down")):
-		dir.y = 1;
-	elif (Input.is_action_pressed("move_up")):
-		dir.y = -1;
-	elif (Input.is_action_pressed("move_right")):
-		dir.x = 1;
-	elif (Input.is_action_pressed("move_left")):
-		dir.x = -1;
-		
-	block_pos += dir*80*delta
-	get_node("Player").set_pos(block_pos)
+	if not actionTaken:
+		if (Input.is_action_pressed("move_down")):
+			dir.y = 1;
+			actionTaken = true;
+		elif (Input.is_action_pressed("move_up")):
+			dir.y = -1;
+			actionTaken = true;
+		elif (Input.is_action_pressed("move_right")):
+			dir.x = 1;
+			actionTaken = true;
+		elif (Input.is_action_pressed("move_left")):
+			dir.x = -1;
+			actionTaken = true;
+	else:
+		if not (Input.is_action_pressed("move_down") or Input.is_action_pressed("move_up") or Input.is_action_pressed("move_right") or Input.is_action_pressed("move_left")):
+			actionTaken = false;
+	
+	if not kinebody.test_move(dir*size):
+		get_node("Player").move(dir*size)
+
+	#get_node("Player").set_pos(block_pos)
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	size = get_node("TileMap").get_cell_size()
 	set_process(true)
