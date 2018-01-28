95663f3e09ff248890fc6bdd9d18eacbac917ced
Jeremy Ryan <jryan@olin.edu>
2018-01-28 11:07:21 -0500

    stable

--- Teleport.gd
+++ Teleport.gd
@@ -12,8 +12,9 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
+	if able_to_progress():
+		get_tree().change_scene(get_parent().next_level)
 	var Teleport = get_node("AnimationPlayer")
-	checkPlayerPos()
 	if not Teleport.is_playing():
 		if not active:
 			Teleport.play("Inactive")
@@ -30,14 +31,20 @@ func getState():
 	return active
 	
 func checkPlayerPos():
-	var granny = get_parent()
-	print(granny)
-	var player_pos = granny.find_node("Player").get_global_pos()
-	var tele_pos = get_global_pos()
-	var offset = Vector2(0, 8)
-	var diff = tele_pos - player_pos - offset
+	var mother = get_parent()
+	var granny = get_parent().get_parent()
+	var player_pos = Vector2(0, 0)
+	if granny.get_name() == "Game":
+		player_pos = granny.find_node("Player").get_global_pos()
+	elif mother.get_name() == "Game":
+		player_pos = mother.find_node("Player").get_global_pos()
+	var pos = get_global_pos()
+	var offset = Vector2(0, -8)
+	var diff = pos - player_pos - offset
 	if diff.length() < 5:
-		print("true")
 		return true
 	else:
 		return false
+		
+func able_to_progress():
+	return(checkPlayerPos() and active)
